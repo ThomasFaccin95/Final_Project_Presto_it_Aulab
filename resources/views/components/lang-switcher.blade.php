cc8d1216f95ceb0e3b90de9891bf7025284d2a52
simone sanseverino <simonesanseverino45@gmail.com>
2026-03-26 16:03:49 +0100

    Replace lang switcher flags with Bootstrap dropdown

--- resources/views/components/lang-switcher.blade.php
+++ resources/views/components/lang-switcher.blade.php
@@ -4,14 +4,31 @@
         'en' => 'https://flagcdn.com/w40/gb.png',
         'es' => 'https://flagcdn.com/w40/es.png',
     ];
+    $labels = [
+        'it' => 'Italiano',
+        'en' => 'English',
+        'es' => 'Español',
+    ];
     $current = app()->getLocale();
 @endphp
 
-<div class="lang-switcher">
-    @foreach (config('app.available_locales') as $locale)
-        <a href="{{ route('lang.switch', $locale) }}" class="lang-btn {{ $current === $locale ? 'active' : '' }}"
-            title="{{ strtoupper($locale) }}">
-            <img src="{{ $flags[$locale] }}" alt="{{ strtoupper($locale) }}" class="lang-flag">
-        </a>
-    @endforeach
+<div class="dropdown">
+    {{-- Bottone con bandiera corrente --}}
+    <button class="lang-dropdown-btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="{{ $flags[$current] }}" alt="{{ strtoupper($current) }}" class="lang-flag">
+        {{ strtoupper($current) }}
+    </button>
+
+    {{-- Menu con tutte le lingue --}}
+    <ul class="dropdown-menu dropdown-menu-end lang-dropdown-menu">
+        @foreach ($flags as $locale => $flag)
+            <li>
+                <a href="{{ route('lang.switch', $locale) }}"
+                    class="dropdown-item lang-dropdown-item {{ $current === $locale ? 'active' : '' }}">
+                    <img src="{{ $flag }}" alt="{{ strtoupper($locale) }}" class="lang-flag">
+                    {{ $labels[$locale] }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
 </div>
